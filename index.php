f0a25075036205573f68b4b3815f11fb0995340f
Willames Santos <willamessantos@live.com>
2022-07-19 23:12:29 -0300

    Add lógica de adicionar evento em todos os botoes de atualizar e trazer os dados para o form de update

--- index.php
+++ index.php
@@ -10,11 +10,12 @@ require('db/connect.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <title>Inserindo dados no DB</title>
     <style>
         table{
             border-collapse: collapse;
-            width: 90%;
+            width: 100%;
         }
         th, td{
             padding: 6px;
@@ -23,13 +24,36 @@ require('db/connect.php');
         }
     </style>
 </head>
-<body style="font-family: sans-serif;">
-    <h1>Aula Inserindo Dados.</h1>
+<body class="container">
+    <h1 class="mt-3">Aula Inserindo Dados.</h1>
     <form method="POST">
-        <input type="text" name="name" placeholder="Digite seu nome" required autofocus>
-        <input type="email" name="email" placeholder="Digite seu email" required>
-        <button type="submit" name="salve">Salvar</button>
+        <input type="text" name="name" placeholder="Digite seu nome" required autofocus class="form-control form-control-sm mt-3">
+        <input type="email" name="email" placeholder="Digite seu email" required class="form-control form-control-sm mt-3">
+        <button type="submit" name="salve" class="btn btn-primary btn-sm mt-3">Salvar</button>
     </form>
+
+    <!-- Modal -->
+    <div class="modal fade" id="atualizarCliente" tabindex="-1" aria-labelledby="atualizarModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="atualizarModalLabel">Atualizar Cliente</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            <form method="POST">
+                <input type="text" name="id-editado" id="id-editado" placeholder="ID" required autofocus class="form-control form-control-sm mt-3">
+                <input type="text" name="name-editado" id="name-editado" placeholder="Digite seu nome" required autofocus class="form-control form-control-sm mt-3">
+                <input type="email" name="email-editado" id="email-editado" placeholder="Digite seu email" required class="form-control form-control-sm mt-3">
+                <button type="submit" id="atualizar" name="atualizar" class="btn btn-primary btn-sm mt-3">Atualizar</button>
+            </form>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Desistir</button>
+        </div>
+        </div>
+    </div>
+    </div>
     <br>
     <?php
         //INSERINDO DADO NO DB DE MANEIRA SIMPLES (VÚLNERÁVEL)
@@ -79,6 +103,46 @@ require('db/connect.php');
     ?>
 
     <?php
+        
+        //PROCESSO DE ATUALIZAR CLIENTE
+        if(isset($_POST['atualizar']) && isset($_POST['name-editado']) && isset($_POST['email-editado']) && isset($_POST['id-editado'])){
+
+            $id = cleanDataPost($_POST['id-editado']);
+            $name = cleanDataPost($_POST['name-editado']);
+            $email = cleanDataPost($_POST['email-editado']);
+
+            //VALIDAÇÃO DE CAMPOS VAZIOS
+            if($name == '' || $name == null){
+                echo "<b style='color: red;'>Nome não pode ser vazio</b>";
+                exit();
+            }
+
+            if($email == '' || $email == null){
+                echo "<b style='color: red;'>Email não pode ser vazio</b>";
+                exit();
+            }
+
+            //VALIDAÇÃO DE NOME
+            if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+                echo "<b style='color: red;'>Somente permitido letras e espaços em branco para o nome</b>";
+                exit();
+            }
+
+            //VALIDAÇÃO DO EMAIL
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                echo "<b style='color: red;'>Email inválido</b>";
+                exit();
+            }
+
+            //ATUALIZANDO CADASTRO
+            $sql = $pdo->prepare("UPDATE clientes SET nome=?, email=? WHERE id=?");
+            $sql->execute(array($name, $email, $id));
+            
+        }
+
+    ?>
+
+    <?php
         //SELECIONAR DADOS DA TABELA
         $sql = $pdo->prepare("SELECT * FROM clientes");
         $sql->execute();
@@ -96,11 +160,12 @@ require('db/connect.php');
 
         if(count($datas) > 0){
 
-            echo "<br><br><table>
+            echo "<table>
                 <tr>
                     <th>CÓDIGO</th>
                     <th>NOME</th>
                     <th>EMAIL</th>
+                    <th>AÇÕES</th>
                 </tr>";
             
             foreach($datas as $key => $value){
@@ -108,6 +173,7 @@ require('db/connect.php');
                         <td>".$value['id']."</td>
                         <td>".$value['nome']."</td>
                         <td>".$value['email']."</td>
+                        <td><a href='#' id='atualizaCliente' data-bs-toggle='modal' data-id='".$value['id']."' data-nome='".$value['nome']."' data-email='".$value['email']."' data-bs-target='#atualizarCliente'>Atualizar<a/></td>
                     </tr>";
             }
 
@@ -118,5 +184,33 @@ require('db/connect.php');
 
     ?>
     
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <script>
+
+        let data = document.querySelectorAll('#atualizaCliente')
+
+        function changeData(id, name, email){
+
+            document.getElementById('name-editado').value = name
+            document.getElementById('id-editado').value = id
+            document.getElementById('email-editado').value = email
+
+        }
+
+        data.forEach((btn, index) => {
+            
+            btn.addEventListener('click', () => {
+
+                let {id, nome, email} = btn.dataset
+                
+                changeData(id, nome, email)
+            })
+        })
+
+        
+
+        // console.log(data)
+
+    </script>
 </body>
 </html>
